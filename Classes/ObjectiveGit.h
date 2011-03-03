8268992f755024c66396261fcecacc6cd35dff24
joshaber <joshaber@gmail.com>
2011-03-03 14:10:37 -0800

    Added a framework header and made the rest of the headers public.

---
+++ Classes/ObjectiveGit.h
@@ -0,0 +1,20 @@
+/*
+ *  ObjectiveGit.h
+ *  ObjectiveGitFramework
+ *
+ *  Created by Josh Abernathy on 3/3/11.
+ *  Copyright 2011 GitHub, Inc. All rights reserved.
+ *
+ */
+
+#import "GTRepository.h"
+#import "GTLib.h"
+#import "GTWalker.h"
+#import "GTCommit.h"
+#import "GTSignature.h"
+#import "GTTree.h"
+#import "GTBlob.h"
+#import "GTTag.h"
+#import "GTIndex.h"
+#import "GTIndexEntry.h"
+#import "GTReference.h"
