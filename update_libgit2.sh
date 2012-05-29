eb5eec8687c2ddb5965cd5ae8b428f69a84c4431
joshaber <joshaber@gmail.com>
2012-05-29 11:36:30 -0400

    added a script to update & build libgit2

---
+++ update_libgit2.sh
@@ -0,0 +1,26 @@
+set -e
+
+cd "libgit2"
+
+LIBGIT2_BRANCH="development"
+
+git checkout "${LIBGIT2_BRANCH}"
+git pull
+
+if [ -d "build" ]; then
+    rm -rf "build"
+fi
+
+mkdir build
+cd build
+
+cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF ..
+cmake --build .
+
+product="libgit2.a"
+install_path="../../${product}"
+if [ "${product}" -nt "${install_path}" ]; then
+    cp -v "${product}" "${install_path}"
+fi
+
+echo "libgit2 has been updated."
