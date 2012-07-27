4595f7944376cf9d9c4f796c01ab4eb533dfd305
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-26 19:58:34 -0700

    Make GTReference.repository strong (similar to #73)

--- Classes/GTReference.h
+++ Classes/GTReference.h
@@ -43,7 +43,7 @@ typedef enum {
 @interface GTReference : NSObject <GTObject> {}
 
 @property (nonatomic, assign) git_reference *git_reference;
-@property (nonatomic, unsafe_unretained) GTRepository *repository;
+@property (nonatomic, strong) GTRepository *repository;
 @property (nonatomic, readonly) NSString *type;
 @property (nonatomic, readonly) const git_oid *oid;
 @property (nonatomic, readonly, getter=isValid) BOOL valid;
