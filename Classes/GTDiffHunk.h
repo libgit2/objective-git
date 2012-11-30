0f69f51fe54b8f6b0e50cd68979207c63e6dcde5
Danny Greg <danny@github.com>
2012-11-30 14:06:03 +0000

    Sketch GTDiffHunk API

---
+++ Classes/GTDiffHunk.h
@@ -0,0 +1,28 @@
+//
+//  GTDiffHunk.h
+//  ObjectiveGitFramework
+//
+//  Created by Danny Greg on 30/11/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "git2.h"
+
+typedef enum : git_diff_line_t {
+	GTDiffHunkLineOriginContext = GIT_DIFF_LINE_CONTEXT,
+	GTDiffHunkLineOriginAddition = GIT_DIFF_LINE_ADDITION,
+	GTDiffHunkLineOriginDeletion = GIT_DIFF_LINE_DELETION,
+	GTDiffHunkLineOriginAddEOFNewLine = GIT_DIFF_LINE_ADD_EOFNL,
+	GTDiffHunkLineOriginDeleteEOFNewLine = GIT_DIFF_LINE_DEL_EOFNL,
+} GTDiffHunkLineOrigin;
+
+typedef BOOL(^GTDiffHunkLineProcessingBlock)(NSString *lineContent, NSUInteger oldLineNumber, NSUInteger newLineNumber, GTDiffHunkLineOrigin lineOrigin);
+
+@interface GTDiffHunk : NSObject
+
+@property (nonatomic, readonly, strong) NSString *header;
+@property (nonatomic, readonly) NSUInteger lineCount;
+
+- (void)enumerateLinesInHunkWithBlock:(GTDiffHunkLineProcessingBlock)block;
+
+@end
---
+++ Classes/GTDiffHunk.m
@@ -0,0 +1,13 @@
+//
+//  GTDiffHunk.m
+//  ObjectiveGitFramework
+//
+//  Created by Danny Greg on 30/11/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "GTDiffHunk.h"
+
+@implementation GTDiffHunk
+
+@end
