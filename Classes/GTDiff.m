5833ebdee0bb1dc069568ffc07459a990dbf28d7
Danny Greg <danny@github.com>
2012-11-30 19:59:17 +0000

    Lol pointers.

--- Classes/GTDiff.m
+++ Classes/GTDiff.m
@@ -21,28 +21,28 @@ NSString *const GTDiffOptionsMaxSizeKey = @"GTDiffOptionsMaxSizeKey";
 
 @implementation GTDiff
 
-+ (git_diff_options)optionsStructFromDictionary:(NSDictionary *)dictionary {
++ (git_diff_options *)optionsStructFromDictionary:(NSDictionary *)dictionary {
 	if (dictionary == nil) return NULL;
 	
-	git_diff_options newOptions;
+	git_diff_options *newOptions = malloc(sizeof(git_diff_options));
 	
 	NSNumber *flagsNumber = dictionary[GTDiffOptionsFlagsKey];
-	if (flagsNumber != nil) newOptions.flags = (uint32)flagsNumber.unsignedIntegerValue;
+	if (flagsNumber != nil) newOptions->flags = (uint32)flagsNumber.unsignedIntegerValue;
 	
 	NSNumber *contextLinesNumber = dictionary[GTDiffOptionsContextLinesKey];
-	if (contextLinesNumber != nil) newOptions.context_lines = (uint16_t)contextLinesNumber.unsignedIntegerValue;
+	if (contextLinesNumber != nil) newOptions->context_lines = (uint16_t)contextLinesNumber.unsignedIntegerValue;
 	
 	NSNumber *interHunkLinesNumber = dictionary[GTDiffOptionsInterHunkLinesKey];
-	if (interHunkLinesNumber != nil) newOptions.interhunk_lines = (uint16_t)interHunkLinesNumber.unsignedIntegerValue;
+	if (interHunkLinesNumber != nil) newOptions->interhunk_lines = (uint16_t)interHunkLinesNumber.unsignedIntegerValue;
 	
 	NSString *oldPrefix = dictionary[GTDiffOptionsOldPrefixKey];
-	if (oldPrefix != nil) newOptions.old_prefix = (char *)oldPrefix.UTF8String;
+	if (oldPrefix != nil) newOptions->old_prefix = (char *)oldPrefix.UTF8String;
 	
 	NSString *newPrefix = dictionary[GTDiffOptionsNewPrefixKey];
-	if (newPrefix != nil) newOptions.new_prefix = (char *)newPrefix.UTF8String;
+	if (newPrefix != nil) newOptions->new_prefix = (char *)newPrefix.UTF8String;
 	
 	NSNumber *maxSizeNumber = dictionary[GTDiffOptionsMaxSizeKey];
-	if (maxSizeNumber != nil) newOptions.max_size = (uint16_t)maxSizeNumber.unsignedIntegerValue;
+	if (maxSizeNumber != nil) newOptions->max_size = (uint16_t)maxSizeNumber.unsignedIntegerValue;
 	
 	return newOptions;
 }
