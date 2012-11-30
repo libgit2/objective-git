e9cb72f199c545cd207d475df506770cf11167e6
Danny Greg <danny@github.com>
2012-11-30 14:05:54 +0000

    Sketch GTDiffFile API.

---
+++ Classes/GTDiffFile.h
@@ -0,0 +1,18 @@
+//
+//  GTDiffFile.h
+//  ObjectiveGitFramework
+//
+//  Created by Danny Greg on 30/11/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "git2.h"
+
+@interface GTDiffFile : NSObject
+
+@property (nonatomic, readonly) git_diff_file *git_diff_file;
+
+@property (nonatomic, readonly, strong) NSString *path;
+@property (nonatomic, readonly) NSUInteger size;
+
+@end
---
+++ Classes/GTDiffFile.m
@@ -0,0 +1,13 @@
+//
+//  GTDiffFile.m
+//  ObjectiveGitFramework
+//
+//  Created by Danny Greg on 30/11/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "GTDiffFile.h"
+
+@implementation GTDiffFile
+
+@end
