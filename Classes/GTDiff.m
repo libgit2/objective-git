b4f0c1de97ebba77e71c307c027c5227b98af1ae
Danny Greg <danny@github.com>
2012-11-30 14:49:48 +0000

    Implement delta generation.

--- Classes/GTDiff.m
+++ Classes/GTDiff.m
@@ -8,6 +8,16 @@
 
 #import "GTDiff.h"
 
+#import "GTDiffDelta.h"
+
+int GTDiffFilesCallback(void *data, const git_diff_delta *delta, float progress);
+
+@interface GTDiff ()
+
+@property (nonatomic, strong) NSMutableArray *deltasBuilderArray;
+
+@end
+
 @implementation GTDiff
 
 - (instancetype)initWithGitDiffList:(git_diff_list *)diffList {
@@ -24,4 +34,24 @@
 	git_diff_list_free(self.git_diff_list);
 }
 
+#pragma mark - Properties
+
+- (NSArray *)deltas {
+	size_t count = git_diff_num_deltas(self.git_diff_list);
+	self.deltasBuilderArray = [NSMutableArray arrayWithCapacity:count];
+	git_diff_foreach(self.git_diff_list, (__bridge void *)(self.deltasBuilderArray), GTDiffFilesCallback, nil, nil);
+	return [NSArray arrayWithArray:self.deltasBuilderArray];
+}
+
+- (NSUInteger)numberOfDeltasWithType:(GTDiffDeltaType)deltaType {
+	return git_diff_num_deltas_of_type(self.git_diff_list, (git_delta_t)deltaType);
+}
+
 @end
+
+int GTDiffFilesCallback(void *data, const git_diff_delta *delta, float progress) {
+	GTDiffDelta *gtDelta = [[GTDiffDelta alloc] initWithGitDelta:(git_diff_delta *)delta];
+	NSMutableArray *passedArray = (__bridge NSMutableArray *)data;
+	if (passedArray != nil)[passedArray addObject:gtDelta];
+	return 0;
+}
