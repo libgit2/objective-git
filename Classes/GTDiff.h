e1fd8f941ee8866fd833b54904f432c7ab93dc17
Danny Greg <danny@github.com>
2012-12-04 16:53:40 +0000

    Document options consts.

--- Classes/GTDiff.h
+++ Classes/GTDiff.h
@@ -14,11 +14,36 @@
 @class GTRepository;
 @class GTTree;
 
+// An `NSNumber` wrapped `GTDiffOptionsFlags` representing any flags you wish to
+// pass into the initialisation.
 extern NSString *const GTDiffOptionsFlagsKey;
+
+// An `NSNumber` wrapped `NSUInteger` dictating how many context lines above and
+// below each individual hunk.
+//
+// Defaults to 3.
 extern NSString *const GTDiffOptionsContextLinesKey;
+
+// An `NSNumber` wrapped `NSUInteger` dictating the minimum number of lines
+// between diff hunks to merge them into one hunk.
+//
+// Defaults to 0.
 extern NSString *const GTDiffOptionsInterHunkLinesKey;
+
+// An `NSString` to prefix old file names with.
+//
+// Defaults to "a".
 extern NSString *const GTDiffOptionsOldPrefixKey;
+
+// An `NSString` to prefix new file names with.
+//
+// Defaults to "b".
 extern NSString *const GTDiffOptionsNewPrefixKey;
+
+// An `NSNumber` wrapped `NSUInteger` determining the maximum size (in bytes)
+// of a file to diff. Above this size the file will be treated as binary.
+//
+// Defaults to 512Mb.
 extern NSString *const GTDiffOptionsMaxSizeKey;
 
 typedef enum : git_diff_option_t {
