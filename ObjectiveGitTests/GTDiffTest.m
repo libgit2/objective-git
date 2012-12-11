31c91e0a77073a31cd43410ddbaa734fe62e783a
Danny Greg <danny@github.com>
2012-12-11 19:09:08 +0000

    Flesh out initialisation tests.

--- ObjectiveGitTests/GTDiffTest.m
+++ ObjectiveGitTests/GTDiffTest.m
@@ -27,7 +27,7 @@
 	STAssertNotNil(self.repository, @"Failed to initialise repository.");
 }
 
-- (void)testDiffingTrees {
+- (void)testInitialisation {
 	GTCommit *firstCommit = [self findCommitWithSHA:@"5b5b025afb0b4c913b4c338a42934a3863bf3644"];
 	GTCommit *secondCommit = [self findCommitWithSHA:@"36060c58702ed4c2a40832c51758d5344201d89a"];
 	STAssertNotNil(firstCommit, @"Could not find first commit to diff against");
@@ -35,8 +35,17 @@
 	
 	GTTree *firstTree = firstCommit.tree;
 	GTTree *secondTree = secondCommit.tree;
-	GTDiff *diff = [GTDiff diffOldTree:firstTree withNewTree:secondTree options:nil];
-	STAssertNotNil(diff, @"Unable to create a diff object with 2 trees.");
+	GTDiff *treeDiff = [GTDiff diffOldTree:firstTree withNewTree:secondTree options:nil];
+	STAssertNotNil(treeDiff, @"Unable to create a diff object with 2 trees.");
+	
+	GTDiff *indexDiff = [GTDiff diffIndexToTree:firstTree options:nil];
+	STAssertNotNil(indexDiff, @"Unable to create a diff object with a tree to an index.");
+	
+	GTDiff *workingDirectoryToIndexDiff = [GTDiff diffWorkingDirectoryToIndexInRepository:self.repository options:nil];
+	STAssertNotNil(workingDirectoryToIndexDiff, @"Unable to create a diff object of the index and working directory.");
+	
+	GTDiff *workingDirectoryToTreeDiff = [GTDiff diffWorkingDirectoryToTree:firstTree options:nil];
+	STAssertNotNil(workingDirectoryToTreeDiff, @"Unable to create a diff with the working directory and a tree.");
 }
 
 @end
