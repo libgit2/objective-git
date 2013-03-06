c686ac71a9d7be22b0df9d6858bdd12c1ec0fe70
Peter Kelly <peter@uxproductivity.com>
2013-03-06 20:57:54 +0700

    Fixed GTIndex entryWithName: + added error arg
    Previously, GTIdex's entryWithName: method treated the result of
    git_index_find() as the position of the entry in the index. However,
    this is wrong - the returned value is an error code (or zero for
    success). So every time you called this method with any name, it would
    return either zero or a negative error code, and the subsequent call to
    git_index_get_byindex() would return the first entry.
    
    This change modifies the behaviour to pass in a pointer to a position
    variable of type size_t in which the position is stored by
    git_index_find() on success, and this value is used to look up the index
    entry. On error, the method returns nil.
    
    I have also added one new method, entryWithName:error:, which allows the
    caller to receive details of the lookup error, in case of failure.

--- Classes/GTIndex.m
+++ Classes/GTIndex.m
@@ -97,8 +97,18 @@
 }
 
 - (GTIndexEntry *)entryWithName:(NSString *)name {
-	int i = git_index_find(0, self.git_index, [name UTF8String]);
-	return [GTIndexEntry indexEntryWithEntry:git_index_get_byindex(self.git_index, (unsigned int)i)];
+	return [self entryWithName: name error: nil];
+}
+
+- (GTIndexEntry *)entryWithName:(NSString *)name error:(NSError **)error {
+	size_t pos = 0;
+	int gitError = git_index_find(&pos, self.git_index, [name UTF8String]);
+	if (gitError != 0) {
+		if(error != NULL)
+			*error = [NSError git_errorFor:gitError withAdditionalDescription:@"Entry not found in index"];
+		return nil;
+	}
+	return [GTIndexEntry indexEntryWithEntry:git_index_get_byindex(self.git_index, pos)];
 }
 
 - (BOOL)addEntry:(GTIndexEntry *)entry error:(NSError **)error {
