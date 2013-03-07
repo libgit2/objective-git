06fd583ca1f6eb4f4bbf965c33e98241423036a7
Peter Kelly <peter@uxproductivity.com>
2013-03-07 20:36:32 +0700

    GIIndex entryWithName: whitespace + error msg fixes

--- Classes/GTIndex.m
+++ Classes/GTIndex.m
@@ -114,13 +114,15 @@
 
 - (GTIndexEntry *)entryWithName:(NSString *)name error:(NSError **)error {
 	size_t pos = 0;
-	int gitError = git_index_find(&pos, self.git_index, [name UTF8String]);
+	int gitError = git_index_find(&pos, self.git_index, name.UTF8String);
 	if (gitError != 0) {
-		if(error != NULL)
-			*error = [NSError git_errorFor:gitError withAdditionalDescription:@"Entry not found in index"];
-		return nil;
+		if (error != NULL) {
+			NSString *desc = [NSString stringWithFormat: @"%@ not found in index", name];
+			*error = [NSError git_errorFor: gitError withAdditionalDescription: desc];
+		}
+		return NULL;
 	}
-	return [GTIndexEntry indexEntryWithEntry:git_index_get_byindex(self.git_index, pos)];
+	return [GTIndexEntry indexEntryWithEntry: git_index_get_byindex(self.git_index, pos)];
 }
 
 - (BOOL)addEntry:(GTIndexEntry *)entry error:(NSError **)error {
