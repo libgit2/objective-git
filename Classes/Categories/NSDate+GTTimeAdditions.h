d871acabc10e21b06f1c48a231bfcc0609fec11c
Danny Greg <danny@github.com>
2013-03-28 10:35:11 +0000

    Re-sketch the API to keep time zones separate.

--- Classes/Categories/NSDate+GTTimeAdditions.h
+++ Classes/Categories/NSDate+GTTimeAdditions.h
@@ -12,17 +12,25 @@
 
 @interface NSDate (GTTimeAdditions)
 
-// The date represented as a `git_time`.
-@property (nonatomic, readonly) git_time gt_gitTime;
-
-// The difference, in minutes, between the current default timezone and GMT. 
-@property (nonatomic, readonly) int gt_gitTimeOffset;
-
 // Creates a new `NSDate` from the provided `git_time`.
 //
-// Note: the date will take into account the timezone offset and return a date
-//       as it was in the source timezone. This matches output from the likes of
-//       `git log`.
-+ (NSDate *)gt_dateFromGitTime:(git_time)time;
+// time     - The `git_time` to base the returned date on.
+// timeZone - The timezone used by the time passed in.
+//
+// Returns an `NSDate` object representing the passed in `time`. 
++ (NSDate *)gt_dateFromGitTime:(git_time)time timeZone:(NSTimeZone **)timeZone;
+
+// Converts the date to a `git_time`.
+//
+// timeZone - An `NSTimeZone` to describe the time offset. This is optional, if
+//            `nil` the default time zone will be used.
+- (git_time)gt_gitTimeUsingTimeZone:(NSTimeZone *)timeZone;
+
+@end
+
+@interface NSTimeZone (GTTimeAdditions)
+
+// The difference, in minutes, between the current default timezone and GMT.
+@property (nonatomic, readonly) int gt_gitTimeOffset;
 
 @end
