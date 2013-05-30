4ac49a43f73510050f5b3fbbfd855cfb0513dcfe
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-05-30 02:25:57 -0700

    Tests for GTRepository submodule enumeration

--- ObjectiveGitTests/GTSubmoduleSpec.m
+++ ObjectiveGitTests/GTSubmoduleSpec.m
@@ -15,4 +15,47 @@ beforeEach(^{
 	expect(repo).notTo.beNil();
 });
 
+it(@"should enumerate top-level submodules", ^{
+	NSMutableSet *names = [NSMutableSet set];
+	[repo enumerateSubmodulesRecursively:NO usingBlock:^(GTSubmodule *submodule, BOOL *stop) {
+		expect(stop).notTo.beNil();
+
+		expect(submodule).to.beKindOf(GTSubmodule.class);
+		expect(submodule.name).notTo.beNil();
+
+		[names addObject:submodule.name];
+	}];
+
+	NSSet *expectedNames = [NSSet setWithArray:@[ @"Archimedes", @"Test_App", @"Test_App2" ]];
+	expect(names).to.equal(expectedNames);
+});
+
+it(@"should enumerate submodules recursively", ^{
+	NSMutableSet *names = [NSMutableSet set];
+	[repo enumerateSubmodulesRecursively:YES usingBlock:^(GTSubmodule *submodule, BOOL *stop) {
+		expect(stop).notTo.beNil();
+
+		expect(submodule).to.beKindOf(GTSubmodule.class);
+		expect(submodule.name).notTo.beNil();
+
+		[names addObject:submodule.name];
+	}];
+
+	NSSet *expectedNames = [NSSet setWithArray:@[ @"Archimedes", @"Configuration", @"ArchimedesTests/expecta", @"ArchimedesTests/specta", @"Test_App", @"Test_App2" ]];
+	expect(names).to.equal(expectedNames);
+});
+
+it(@"should terminate enumeration early", ^{
+	__block NSUInteger count = 0;
+	[repo enumerateSubmodulesRecursively:NO usingBlock:^(GTSubmodule *submodule, BOOL *stop) {
+		if (count == 2) {
+			*stop = YES;
+		} else {
+			count++;
+		}
+	}];
+
+	expect(count).to.equal(2);
+});
+
 SpecEnd
