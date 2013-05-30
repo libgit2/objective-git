9b5ba7405b9861c7328a886ec12218efeb23d7fb
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-05-30 02:40:04 -0700

    Tests for a specific submodule

--- ObjectiveGitTests/GTSubmoduleSpec.m
+++ ObjectiveGitTests/GTSubmoduleSpec.m
@@ -58,4 +58,45 @@ it(@"should terminate enumeration early", ^{
 	expect(count).to.equal(2);
 });
 
+describe(@"clean, checked out submodule", ^{
+	__block GTSubmodule *submodule;
+
+	beforeEach(^{
+		NSError *error = nil;
+		submodule = [repo submoduleWithName:@"Test_App" error:&error];
+		expect(submodule).notTo.beNil();
+		expect(error).to.beNil();
+
+		expect(submodule.name).to.equal(@"Test_App");
+		expect(submodule.repository).to.beIdenticalTo(repo);
+		expect(submodule.git_submodule).notTo.beNil();
+	});
+
+	it(@"should have identical OIDs", ^{
+		expect(submodule.indexOID.SHA).to.equal(@"f7ecd8f4404d3a388efbff6711f1bdf28ffd16a0");
+		expect(submodule.HEADOID.SHA).to.equal(@"f7ecd8f4404d3a388efbff6711f1bdf28ffd16a0");
+		expect(submodule.workingDirectoryOID.SHA).to.equal(@"f7ecd8f4404d3a388efbff6711f1bdf28ffd16a0");
+	});
+
+	it(@"should have a clean status", ^{
+		GTSubmoduleStatus expectedStatus = GTSubmoduleStatusExistsInHEAD | GTSubmoduleStatusExistsInIndex | GTSubmoduleStatusExistsInConfig | GTSubmoduleStatusExistsInWorkingDirectory;
+
+		__block NSError *error = nil;
+		expect([submodule statusWithError:&error]).to.equal(expectedStatus);
+		expect(error).to.beNil();
+	});
+
+	it(@"should open a repository" ,^{
+		NSError *error = nil;
+		GTRepository *submoduleRepo = [submodule submoduleRepositoryWithError:&error];
+		expect(submoduleRepo).notTo.beNil();
+		expect(error).to.beNil();
+
+		expect(submoduleRepo.fileURL).to.equal([repo.fileURL URLByAppendingPathComponent:@"Test_App"]);
+		expect(submoduleRepo.bare).to.beFalsy();
+		expect(submoduleRepo.empty).to.beFalsy();
+		expect(submoduleRepo.headDetached).to.beTruthy();
+	});
+});
+
 SpecEnd
