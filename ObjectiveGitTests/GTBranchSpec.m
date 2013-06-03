f2b5a447b21bb0ef0785db3af7f1125e1c6a0a8b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-06-03 13:32:31 -0700

    Test GTBranch.shortName

--- ObjectiveGitTests/GTBranchSpec.m
+++ ObjectiveGitTests/GTBranchSpec.m
@@ -10,21 +10,39 @@
 
 SpecBegin(GTBranch)
 
+__block GTRepository *repository;
+__block GTBranch *masterBranch;
+
+beforeEach(^{
+	repository = [self fixtureRepositoryNamed:@"Test_App"];
+	expect(repository).notTo.beNil();
+
+	NSError *error = nil;
+	masterBranch = [repository currentBranchWithError:&error];
+	expect(masterBranch).notTo.beNil();
+	expect(error).to.beNil();
+});
+
+describe(@"shortName", ^{
+	it(@"should use just the branch name for a local branch", ^{
+		expect(masterBranch.shortName).to.equal(@"master");
+	});
+
+	it(@"should not include the remote name for a tracking branch", ^{
+		GTBranch *trackingBranch = [masterBranch trackingBranchWithError:NULL success:NULL];
+		expect(trackingBranch).notTo.equal(masterBranch);
+		expect(trackingBranch.shortName).to.equal(@"master");
+	});
+});
+
 describe(@"-calculateAhead:behind:relativeTo:error:", ^{
 	it(@"should report the right numbers", ^{
-		GTRepository *repository = [self fixtureRepositoryNamed:@"Test_App"];
-		expect(repository).notTo.beNil();
-
-		GTBranch *currentBranch = [repository currentBranchWithError:NULL];
-		expect(currentBranch).notTo.beNil();
-		expect(currentBranch.shortName).to.equal(@"master");
-
-		GTBranch *trackingBranch = [currentBranch trackingBranchWithError:NULL success:NULL];
+		GTBranch *trackingBranch = [masterBranch trackingBranchWithError:NULL success:NULL];
 		expect(trackingBranch).notTo.beNil();
 
 		size_t ahead = 0;
 		size_t behind = 0;
-		[currentBranch calculateAhead:&ahead behind:&behind relativeTo:trackingBranch error:NULL];
+		[masterBranch calculateAhead:&ahead behind:&behind relativeTo:trackingBranch error:NULL];
 		expect(ahead).to.equal(9);
 		expect(behind).to.equal(0);
 	});
