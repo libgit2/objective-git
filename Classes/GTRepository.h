c2a1892b3c6a8567f42753d1ea0746d53ef70e86
joshaber <joshaber@gmail.com>
2013-06-19 11:07:09 -0400

    first pass at some obvious thread safety problems

--- Classes/GTRepository.h
+++ Classes/GTRepository.h
@@ -56,7 +56,7 @@ enum {
 
 typedef unsigned int GTRepositoryFileStatus;
 
-typedef enum : int {
+typedef enum {
     GTRepositoryResetTypeSoft = GIT_RESET_SOFT,
     GTRepositoryResetTypeMixed = GIT_RESET_MIXED,
     GTRepositoryResetTypeHard = GIT_RESET_HARD
