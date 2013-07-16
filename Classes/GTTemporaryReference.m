c12e6b7ff3740ae3b438f4cff7877d9c3ee2794d
Etienne Samson <tiennou7@gmail.com>
2013-07-16 14:24:02 +0200

    Build API.

--- Classes/GTTemporaryReference.m
+++ Classes/GTTemporaryReference.m
@@ -28,4 +28,72 @@
 
 	return self;
 }
+
+- (git_reference *)git_reference {
+	return NULL;
+}
+
+- (const git_oid *)git_oid {
+	return self.oid.git_oid;
+}
+
+- (GTReferenceType)referenceType {
+	return GTReferenceTypeInvalid;
+}
+
+- (id)unresolvedTarget {
+	return self.resolvedTarget;
+}
+
+- (id)resolvedTarget {
+	return [self.repository lookupObjectByOID:self.oid error:NULL];
+}
+
+- (GTReference *)resolvedReference {
+	return nil;
+}
+
+//- (NSString *)SHA
+- (GTReflog *)reflog {
+	return nil;
+}
+
+- (GTReference *)referenceByRenaming:(NSString *)newName error:(NSError **)error {
+	if (error != NULL) *error = [NSError git_errorFor:GIT_ENOTFOUND description:@"Failed to rename reference" failureReason:@"Temporary references can't be renamed"];
+	return nil;
+}
+
+- (GTReference *)referenceByUpdatingTarget:(NSString *)newTarget error:(NSError **)error {
+	if (error != NULL) *error = [NSError git_errorFor:GIT_ENOTFOUND description:@"Failed to update reference" failureReason:@"Temporary references can't be updated"];
+	return nil;
+}
+
+- (BOOL)deleteWithError:(NSError **)error {
+	return YES;
+}
+
+- (GTReference *)resolvedReferenceWithError:(NSError **)error {
+	if (error != NULL) *error = [NSError git_errorFor:GIT_ENOTFOUND description:@"Failed to resolve reference" failureReason:@"Temporary references can't be resolved"];
+	return nil;
+}
+
+- (GTReference *)reloadedReferenceWithError:(NSError **)error {
+	if (error != NULL) *error = [NSError git_errorFor:GIT_ENOTFOUND description:@"Failed to reload reference" failureReason:@"Temporary references can't be reloaded"];
+	return nil;
+}
+
+#pragma mark NSObject
+
+- (NSUInteger)hash {
+	return self.oid.hash;
+}
+
+- (BOOL)isEqual:(GTReference *)reference {
+	if (self == reference) return YES;
+	if (![reference isKindOfClass:GTReference.class]) return NO;
+	
+	return [self.unresolvedTarget isEqual:reference.unresolvedTarget];
+}
+
+
 @end
