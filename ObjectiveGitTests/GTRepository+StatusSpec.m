b09f4b9eb1aa28483341851ee2a3fda2bd2438f8
Danny Greg <danny@github.com>
2013-08-09 12:12:30 -0700

    Add tests for untracked, modified and deleted files.
    This is just in the working directory right now. Need to figure out the
    best way to change stages in this test.

--- ObjectiveGitTests/GTRepository+StatusSpec.m
+++ ObjectiveGitTests/GTRepository+StatusSpec.m
@@ -10,21 +10,40 @@ SpecBegin(GTRepositoryStatus)
 
 describe(@"Checking status", ^{
 	__block GTRepository *repository = nil;
+	NSData *testData = [@"test" dataUsingEncoding:NSUTF8StringEncoding];
+	NSURL *targetFileURL = [repository.fileURL URLByAppendingPathComponent:@"main.m"];
+	
 	beforeEach(^{
 		repository = [GTRepository repositoryWithURL:[NSURL fileURLWithPath:TEST_APP_REPO_PATH(self.class)] error:NULL];
 		expect(repository).toNot.beNil();
 	});
 	
 	it(@"should recognise untracked files", ^{
-		[repository enumerateFileStatusWithOptions:nil usingBlock:^(GTStatusDelta *headToIndex, GTStatusDelta *indexToWorkingDirectory, BOOL *stop) {
-			if ([indexToWorkingDirectory.newFile.path isEqualToString:@"UntrackedImage.png"]) {
-				expect(indexToWorkingDirectory.status).to.equal(GTStatusDeltaStatusUntracked);
-			}
+		[repository enumerateFileStatusWithOptions:nil usingBlock:^(GTStatusDelta *headToIndex, GTStatusDelta *indexToWorkingDirectory, BOOL *stop) {\
+			if (![indexToWorkingDirectory.newFile.path isEqualToString:@"UntrackedImage.png"]) return;
+			expect(indexToWorkingDirectory.status).to.equal(GTStatusDeltaStatusUntracked);
 		}];
 	});
 	
 	it(@"should recognise added files", ^{
-		
+		//TODO: figure out the best way to stage files for this test.
+	});
+	
+	it(@"should recognise modified files", ^{
+		[NSFileManager.defaultManager removeItemAtURL:targetFileURL error:NULL];
+		[testData writeToURL:targetFileURL atomically:YES];
+		[repository enumerateFileStatusWithOptions:nil usingBlock:^(GTStatusDelta *headToIndex, GTStatusDelta *indexToWorkingDirectory, BOOL *stop) {
+			if (![indexToWorkingDirectory.newFile.path isEqualToString:targetFileURL.lastPathComponent]) return;
+			expect(indexToWorkingDirectory.status).to.equal(GTStatusDeltaStatusModified);
+		}];
+	});
+	
+	it(@"should recognise deleted files", ^{
+		[NSFileManager.defaultManager removeItemAtURL:targetFileURL error:NULL];
+		[repository enumerateFileStatusWithOptions:nil usingBlock:^(GTStatusDelta *headToIndex, GTStatusDelta *indexToWorkingDirectory, BOOL *stop) {
+			if (![indexToWorkingDirectory.newFile.path isEqualToString:targetFileURL.lastPathComponent]) return;
+			expect(indexToWorkingDirectory.status).to.equal(GTStatusDeltaStatusDeleted);
+		}];
 	});
 });
 
