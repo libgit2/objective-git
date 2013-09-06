4c8b62ee23f5555cb9b959f20f48b494d915561f
Etienne Samson <tiennou7@gmail.com>
2013-09-06 23:42:54 +0200

    Tests for `GTRemote`.
    Failing at the moment...

--- ObjectiveGitTests/GTRemoteSpec.m
+++ ObjectiveGitTests/GTRemoteSpec.m
@@ -31,30 +31,72 @@
 
 SpecBegin(GTRemote)
 
+__block NSURL *fetchingRepoURL;
 __block GTRepository *masterRepo;
 __block GTRepository *fetchingRepo;
+__block NSArray *remoteNames;
+__block NSString *remoteName;
 
 beforeEach(^{
 	masterRepo = [self fixtureRepositoryNamed:@"testrepo.git"];
 	expect(masterRepo).notTo.beNil();
-	
-	/* Build back an URL to the temporary fixture folder
-	 * I'd wanted to do :
-	 
-	 NSURL *masterRepoURL = [NSURL fileURLWithPath:[self pathForFixtureRepositoryNamed:@"testrepo.git"]];
-	 NSURL *fixturesURL = [NSURL fileURLWithPath:self.repositoryFixturesPath];
-	 
-	 * But this errors.
-	 */
-	
+
 	NSURL *masterRepoURL = [masterRepo fileURL];
 	NSURL *fixturesURL = [masterRepoURL URLByDeletingLastPathComponent];
-	NSURL *fetchingURLRepo = [fixturesURL URLByAppendingPathComponent:@"fetchrepo"];
+	fetchingRepoURL = [fixturesURL URLByAppendingPathComponent:@"fetchrepo"];
 
 	NSError *error = nil;
-	fetchingRepo = [GTRepository cloneFromURL:masterRepoURL toWorkingDirectory:fetchingURLRepo barely:NO withCheckout:YES error:&error transferProgressBlock:nil checkoutProgressBlock:nil];
+	fetchingRepo = [GTRepository cloneFromURL:masterRepoURL toWorkingDirectory:fetchingRepoURL barely:NO withCheckout:YES error:&error transferProgressBlock:nil checkoutProgressBlock:nil];
 	expect(fetchingRepo).notTo.beNil();
 	expect(error.localizedDescription).to.beNil();
+
+	remoteNames = [fetchingRepo remoteNamesWithError:&error];
+	expect(error.localizedDescription).to.beNil();
+	expect(remoteNames.count).to.beGreaterThanOrEqualTo(@(1));
+
+	remoteName = [remoteNames objectAtIndex:0];
+});
+
+afterEach(^{
+	[[NSFileManager defaultManager] removeItemAtURL:fetchingRepoURL error:nil];
+});
+
+describe(@"-remoteWithName:inRepository:error", ^{
+	it(@"should return existing remotes", ^{
+		NSError *error = nil;
+
+		GTRemote *originRemote = [GTRemote remoteWithName:remoteName inRepository:fetchingRepo error:&error];
+		expect(error.localizedDescription).to.beNil();
+		expect(originRemote).notTo.beNil();
+	});
+
+	it(@"should fail for non-existent remotes", ^{
+		NSError *error = nil;
+
+		GTRemote *originRemote = [GTRemote remoteWithName:@"blork" inRepository:fetchingRepo error:&error];
+		expect(error.localizedDescription).notTo.beNil();
+		expect(originRemote).to.beNil();
+	});
+});
+
+describe(@"-createRemoteWithName:url:inRepository:error", ^{
+	it(@"should allow creating new remotes", ^{
+		NSError *error = nil;
+		GTRemote *remote = [GTRemote createRemoteWithName:@"newremote" url:@"git://user@example.com/testrepo.git" inRepository:fetchingRepo	error:&error];
+		expect(error.localizedDescription).to.beNil();
+		expect(remote).notTo.beNil();
+	});
+});
+
+describe(@"-fetchWithError:credentials:progress:", ^{
+	it(@"allows remotes to be fetched", ^{
+		NSError *error = nil;
+		GTRemote *remote = [GTRemote remoteWithName:remoteName inRepository:fetchingRepo error:nil]; // Tested above
+
+		BOOL result = [remote fetchWithError:&error credentials:nil progress:nil];
+		expect(error.localizedDescription).to.beNil();
+		expect(result).to.beTruthy();
+	});
 });
 
 SpecEnd
