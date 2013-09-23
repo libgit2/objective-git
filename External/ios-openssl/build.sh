937a49eed126f24f80a78a575e216da2b5fe432d
Isaac Kearse <isaac@kearse.co.nz>
2013-09-23 21:56:45 +1200

    Update iOS build scripts to include arm64

--- External/ios-openssl/build.sh
+++ External/ios-openssl/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Yay shell scripting! This script builds a static version of
-# OpenSSL ${OPENSSL_VERSION} for iOS 5.1 that contains code for armv6, armv7 and i386.
+# OpenSSL ${OPENSSL_VERSION} for iOS 7.0 that contains code for armv6, armv7, arm64 and i386.
 
 set -x
 
@@ -41,7 +41,7 @@ build()
    cd "/tmp/openssl"
    ./Configure BSD-generic32 no-gost --openssldir="/tmp/openssl-${ARCH}" &> "/tmp/openssl-${ARCH}.log"
    perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
-   perl -i -pe "s|^CC= gcc|CC= ${CLANG} -arch ${ARCH}|g" Makefile
+   perl -i -pe "s|^CC= gcc|CC= ${CLANG} -miphoneos-version-min=${SDK_VERSION} -arch ${ARCH}|g" Makefile
    perl -i -pe "s|^CFLAG= (.*)|CFLAG= -isysroot ${SDK} \$1|g" Makefile
    make &> "/tmp/openssl-${ARCH}.log"
    make install &> "/tmp/openssl-${ARCH}.log"
@@ -51,6 +51,7 @@ build()
 
 build "armv7" "${IPHONEOS_SDK}"
 build "armv7s" "${IPHONEOS_SDK}"
+build "arm64" "${IPHONEOS_SDK}"
 build "i386" "${IPHONESIMULATOR_SDK}"
 
 #
@@ -62,11 +63,13 @@ mkdir External/ios-openssl/lib
 lipo \
 	"/tmp/openssl-armv7/lib/libcrypto.a" \
 	"/tmp/openssl-armv7s/lib/libcrypto.a" \
+	"/tmp/openssl-arm64/lib/libcrypto.a" \
 	"/tmp/openssl-i386/lib/libcrypto.a" \
 	-create -output External/ios-openssl/lib/libcrypto.a
 lipo \
 	"/tmp/openssl-armv7/lib/libssl.a" \
 	"/tmp/openssl-armv7s/lib/libssl.a" \
+	"/tmp/openssl-arm64/lib/libssl.a" \
 	"/tmp/openssl-i386/lib/libssl.a" \
 	-create -output External/ios-openssl/lib/libssl.a
 
