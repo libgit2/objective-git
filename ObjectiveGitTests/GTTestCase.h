00c6abd03e114c278e5713ffbfa086476238b64b
Danny Greg <danny@github.com>
2013-10-08 12:29:00 -0700

    Remove our duplicated fixture setup.
    We seemed to have 2 completely different codepaths, which depending on
    which you picked, would lead to a different set of fixtures being used
    by your test. This was also the root cause of our faulty clean up.
    
    Now there is but one set of fixtures and it's set up and torn down
    properly.

--- ObjectiveGitTests/GTTestCase.h
+++ ObjectiveGitTests/GTTestCase.h
@@ -11,21 +11,19 @@
 
 @class GTRepository;
 
-BOOL unzipFileFromArchiveAtPathIntoDirectory(NSString *fileName, NSString *zipPath, NSString *destinationPath);
-NSString *repositoryFixturePathForName(NSString *repositoryName, Class cls);
-BOOL setupRepositoryFixtureIfNeeded(NSString *repositoryName, Class cls);
-NSString *TEST_REPO_PATH(Class cls);
-NSString *TEST_INDEX_PATH(Class cls);
-NSString *TEST_APP_REPO_PATH(Class cls);
-void rm_loose(Class cls, NSString *sha);
-
 @interface GTTestCase : SPTSenTestCase
 
 // The file URL for a temporary directory which will live for the length of each
 // example (`it`).
 @property (nonatomic, readonly, strong) NSURL *tempDirectoryFileURL;
 
-// Find and return the fixture repository with the given name;
-- (GTRepository *)fixtureRepositoryNamed:(NSString *)name;
+// A fully fledged repository, great for testing nearly everything.
+- (GTRepository *)testAppFixtureRepository;
+
+// A bare repository with a minimal history.
+- (GTRepository *)bareFixtureRepository;
+
+// A repository which has a submodule.
+- (GTRepository *)submoduleFixtureRepository;
 
 @end
