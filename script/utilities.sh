5a6fe1c23f987eee81ed5598b5fe50d3ad3dbc25
Alan Rogers <alan@github.com>
2013-10-08 12:43:52 +1100

    Tidy up and document the xcode_version functions.

--- script/utilities.sh
+++ script/utilities.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-function xcode_major_version () {
-  local XCODE_VERSION=`/usr/bin/xcodebuild -version | head -n 1 | awk '{ print $2 }'`
+# Returns the version # of xcodebuild
+# eg. (4.6.3, 5.0, 5.0.1)
+function xcode_version ()
+{
+    local XCODE_VERSION=`/usr/bin/xcodebuild -version | head -n 1 | awk '{ print $2 }'`
+    echo $XCODE_VERSION
+}
 
-  local XCODE_MAJOR_VERSION=`echo $XCODE_VERSION | awk -F '.' '{ print $1 }'`
+# Returns the major version of xcodebuild
+# eg. (4, 5, 6)
+function xcode_major_version ()
+{
+    local XCODE_MAJOR_VERSION=`echo $(xcode_version) | awk -F '.' '{ print $1 }'`
 
-  echo $XCODE_MAJOR_VERSION
+    echo $XCODE_MAJOR_VERSION
 }
