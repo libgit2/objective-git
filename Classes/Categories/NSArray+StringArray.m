db1952fe7cc979da23e0bcc78ceb04aef7bba4e1
Danny Greg <danny@github.com>
2013-10-07 18:12:17 -0700

    Unnecessary cast --;

--- Classes/Categories/NSArray+StringArray.m
+++ Classes/Categories/NSArray+StringArray.m
@@ -13,7 +13,7 @@
 - (git_strarray)git_strarray {
 	if (self.count < 1) return (git_strarray){ .strings = NULL, .count = 0 };
 	
-	char **cStrings = (char **)malloc(self.count * sizeof(char *));
+	char **cStrings = malloc(self.count * sizeof(char *));
 	for (NSUInteger idx = 0; idx < self.count; idx++) {
 		NSString *string = self[idx];
 		NSAssert([string isKindOfClass:NSString.class], @"A string array must only contain NSStrings. %@ is not a string.", string);
