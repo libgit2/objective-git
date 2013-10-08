e46866eaef233ad9d03562dffae831d56111be84
Alan Rogers <alan@github.com>
2013-10-08 16:02:19 +1100

    Clean up and document the build env variables.

--- script/ios_build_functions.sh
+++ script/ios_build_functions.sh
@@ -5,32 +5,44 @@ source "${SCRIPT_PATH}/xcode_functions.sh"
 
 function setup_build_environment ()
 {
-  pushd . > /dev/null
-  cd "$SCRIPT_PATH/.."
-  ROOT_PATH="$(pwd)"
-  popd > /dev/null
+    pushd . > /dev/null
+    cd "$SCRIPT_PATH/.."
+    ROOT_PATH="$(pwd)"
+    popd > /dev/null
 
-  CLANG=`/usr/bin/xcrun --find clang`
-  CC=${CLANG}
-  CPP="${CLANG} -E"
-  ARCHS=("i386" "armv7" "armv7s")
-  DEVELOPER="/Applications/Xcode.app/Contents/Developer"
-  MACOSX_DEPLOYMENT_TARGET=""
-  
-  XCODE_MAJOR_VERSION=$(xcode_major_version)
+    CLANG=`/usr/bin/xcrun --find clang`
+    CC=${CLANG}
+    CPP="${CLANG} -E"
+    ARCHS=("i386" "armv7" "armv7s")
+    DEVELOPER="/Applications/Xcode.app/Contents/Developer"
+    # We need to clear this so that cmake doesn't have a conniption
+    MACOSX_DEPLOYMENT_TARGET=""
 
-  if [ "${XCODE_MAJOR_VERSION}" -ge "5" ]; then
-    SDKVERSION="7.0"
-    if [ -z "${IPHONEOS_DEPLOYMENT_TARGET}" ]; then
-      IPHONEOS_DEPLOYMENT_TARGET=${SDKVERSION}
+    XCODE_MAJOR_VERSION=$(xcode_major_version)
+
+    # If IPHONEOS_DEPLOYMENT_TARGET has not been specified
+    # setup reasonable defaults to allow running this
+    # of a build script directly (ie not from an Xcode proj)
+
+    if [ "${XCODE_MAJOR_VERSION}" -ge "5" ]
+    then
+        SDKVERSION="7.0"
+        if [ -z "${IPHONEOS_DEPLOYMENT_TARGET}" ]
+        then
+            IPHONEOS_DEPLOYMENT_TARGET=${SDKVERSION}
+        fi
+        # Build 64-bit version only if we are using Xcode 5
+        # and the deployment target is 6.0+
+        if [ `echo ${IPHONEOS_DEPLOYMENT_TARGET} '>=' 6.0 | bc -l` == "1" ]; then
+            ARCHS=("x86_64" "${ARCHS[@]}" "arm64")
+        fi
+    else
+        SDKVERSION="6.1"
+        if [ -z "${IPHONEOS_DEPLOYMENT_TARGET}" ]
+        then
+            IPHONEOS_DEPLOYMENT_TARGET="5.0"
+        fi
     fi
-    if [ `echo ${IPHONEOS_DEPLOYMENT_TARGET} '>=' 6.0 | bc -l` == "1" ]; then
-      ARCHS=("x86_64" "${ARCHS[@]}" "arm64")
-    fi
-  else
-    SDKVERSION="6.1"
-    IPHONEOS_DEPLOYMENT_TARGET="5.0"
-  fi
 }
 
 function build_all_archs 
