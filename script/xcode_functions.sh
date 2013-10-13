6d4672ed981ad0636b0dae4bcd76111aeae3166f
Alan Rogers <alan@github.com>
2013-10-13 17:56:28 +1100

    Redirect stderr to /dev/null for xcodebuild calls.

--- script/xcode_functions.sh
+++ script/xcode_functions.sh
@@ -4,7 +4,7 @@
 # eg. (4.6.3, 5.0, 5.0.1)
 function xcode_version ()
 {
-    /usr/bin/xcodebuild -version | head -n 1 | awk '{ print $2 }'
+    /usr/bin/xcodebuild -version 2> /dev/null | head -n 1 | awk '{ print $2 }'
 }
 
 # Returns the major version of xcodebuild
@@ -20,11 +20,11 @@ function ios_sdk_version ()
 {
     # This relies on the fact that the latest iPhone SDK
     # is the last thing listed before the Xcode version.
-    /usr/bin/xcodebuild -version -sdk | tail -n 4 | head -n 1 | awk '{ print $2 }'
+    /usr/bin/xcodebuild -version -sdk 2> /dev/null | tail -n 4 | head -n 1 | awk '{ print $2 }'
 }
 
 # Returns the path to the specified iOS SDK name
 function ios_sdk_path ()
 {
-    /usr/bin/xcodebuild -version -sdk | grep -i $1 | grep 'Path:' | awk '{ print $2 }'
+    /usr/bin/xcodebuild -version -sdk 2> /dev/null | grep -i $1 | grep 'Path:' | awk '{ print $2 }'
 }
