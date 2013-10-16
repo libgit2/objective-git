4bfd3184e4ff34bdd51a75e33e1a291dd5588921
Alan Rogers <alan@github.com>
2013-10-16 16:34:59 +1100

    Document the properties.

--- Classes/GTRemote.h
+++ Classes/GTRemote.h
@@ -10,13 +10,16 @@
 
 @interface GTRemote : NSObject
 
-@property (nonatomic, readonly, copy) NSString *name;
-@property (nonatomic, readonly, copy) NSString *URLString;
-
 - (id)initWithGitRemote:(git_remote *)remote;
 
 // The underlying `git_remote` object.
 - (git_remote *)git_remote __attribute__((objc_returns_inner_pointer));
+
+// The name of the remote.
+@property (nonatomic, readonly, copy) NSString *name;
+
+// The push and fetch URL for this remote.
+@property (nonatomic, readonly, copy) NSString *URLString;
 
 
 // Updates the URL String for this remote
