70f5611765c12dbf96d380e93d402a373d940578
Alan Rogers <alan@github.com>
2013-10-17 14:39:06 +1100

    Improved GTRemote header docs.

--- Classes/GTRemote.h
+++ Classes/GTRemote.h
@@ -24,34 +24,45 @@
 // The name of the remote.
 @property (nonatomic, readonly, copy) NSString *name;
 
-// The push and fetch URL for this remote.
+// The URL string for the remote.
 @property (nonatomic, readonly, copy) NSString *URLString;
 
 // The fetch refspecs for this remote.
-// Example of a refspec is: @"+refs/heads/*:refs/remotes/%@/*".
+// Example fetch refspec: @"+refs/heads/*:refs/remotes/%@/*".
 //
-// Returns an NSArray of NSStrings.
+// Returns an NSArray of NSString.
 @property (nonatomic, readonly, copy) NSArray *fetchRefSpecs;
 
 // Updates the URL string for this remote.
 //
-// Returns YES if the URLString was succesfully updated.
-// Returns NO and an error if updating failed.
+// URLString - The URLString to update to.
+// error     - If not NULL, this will be set to any error that occurs when
+//             updating the URLString or saving the remote.
+//
+// Returns YES if the URLString was succesfully updated, NO and an error
+// if updating or saving the remote failed.
 - (BOOL)updateURLString:(NSString *)URLString error:(NSError **)error;
 
 // Adds a fetch refspec to this remote.
 //
+// fetchRefSpec - The fetch refSpec string to add.
+// error        - If not NULL, this will be set to any error that occurs
+//                when adding the refSpec or saving the remote.
+//
 // Returns YES if there is the refspec is successflly added
-// or a matching refspec is already present.
-// Returns NO and an error if updating failed.
+// or a matching refspec is already present, NO and an error if
+// adding the refspec or saving the remote failed.
 - (BOOL)addFetchRefSpec:(NSString *)fetchRefSpec error:(NSError **)error;
 
 // Removes the first fetchRefSpec that matches.
 //
+// fetchRefSpec - The fetch refSpec string to remove.
+// error        - If not NULL, this will be set to any error that occurs
+//                when removing the refSpec or saving the remote.
+//
 // Returns YES if the matching refspec is found and removed, or if no matching
-// refspec was found.
-// Returns NO and error if a matching refspec was found but could
-// not be removed.
+// refspec was found. NO and error is returned if a matching refspec was found
+// but could not be removed, or saving the remote failed.
 - (BOOL)removeFetchRefSpec:(NSString *)fetchRefSpec error:(NSError **)error;
 
 @end
