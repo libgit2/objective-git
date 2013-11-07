618570bb0f89717de7fb3715732b58a8e4052ded
Etienne Samson <tiennou7@gmail.com>
2013-11-07 18:36:47 +0100

    Rewrite tests for GTBlob.

---
+++ ObjectiveGitTests/GTBlobSpec.m
@@ -0,0 +1,92 @@
+//
+//  GTBlobSpec.m
+//  ObjectiveGitFramework
+//
+//  Created by Etienne Samson on 2013-11-07.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import "GTBlob.h"
+
+SpecBegin(GTBlob)
+
+__block GTRepository *repository;
+__block NSString *blobSHA;
+__block GTBlob *blob;
+__block NSError *error;
+
+beforeEach(^{
+	error = nil;
+});
+
+describe(@"blob properties can be accessed", ^{
+	beforeEach(^{
+		repository = self.bareFixtureRepository;
+		blobSHA = @"fa49b077972391ad58037050f2a75f74e3671e92";
+		blob = [repository lookupObjectBySHA:blobSHA objectType:GTObjectTypeBlob error:NULL];
+		expect(blob).notTo.beNil();
+	});
+
+	it(@"has a size", ^{
+		expect(blob.size).to.equal(9);
+	});
+
+	it(@"has content", ^{
+		expect(blob.content).to.equal(@"new file\n");
+	});
+
+	it(@"has type", ^{
+		expect(blob.type).to.equal(@"blob");
+	});
+
+	it(@"has a SHA", ^{
+		expect(blob.SHA).to.equal(blobSHA);
+	});
+});
+
+describe(@"blobs can be created", ^{
+	beforeEach(^{
+		repository = self.testAppFixtureRepository;
+	});
+
+	describe(@"+blobWithString:inRepository:error", ^{
+		it(@"works with valid parameters", ^{
+			blob = [GTBlob blobWithString:@"a new blob content" inRepository:repository error:&error];
+			expect(error).to.beNil();
+			expect(blob).notTo.beNil();
+			expect(blob.SHA).notTo.beNil();
+		});
+	});
+
+	describe(@"+blobWithData:inRepository:error", ^{
+		it(@"works with valid parameters", ^{
+			char bytes[] = "100644 example_helper.rb\00\xD3\xD5\xED\x9D A4_\x00 40000 examples";
+			NSData *content = [NSData dataWithBytes:bytes length:sizeof(bytes)];
+
+			blob = [GTBlob blobWithData:content inRepository:repository error:&error];
+			expect(error).to.beNil();
+			expect(blob).notTo.beNil();
+			expect(blob.SHA).notTo.beNil();
+		});
+	});
+
+	describe(@"+blobWithFile:inRepository:error", ^{
+		it(@"works with valid parameters", ^{
+			NSString *fileContent = @"Test contents\n";
+			NSString *fileName = @"myfile.txt";
+			NSURL *fileURL = [repository.fileURL URLByAppendingPathComponent:fileName];
+
+			BOOL success = [fileContent writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:&error];
+			expect(success).to.beTruthy();
+			expect(error).to.beNil();
+
+			blob = [GTBlob blobWithFile:fileURL inRepository:repository error:&error];
+			expect(error).to.beNil();
+			expect(blob).notTo.beNil();
+			expect(blob.SHA).notTo.beNil();
+			expect(blob.content).to.equal(fileContent);
+		});
+	});
+});
+
+SpecEnd
