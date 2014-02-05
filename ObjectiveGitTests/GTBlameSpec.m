168e4fb12d238d0c84b34c23ebc9a5969db1e390
PiersonBro <manit8525@gmail.com>
2014-02-04 21:00:22 -0800

    Add tests for the new options keys.
    Blame support is not fully implemented in libgit2 as such I tried to
    write test that will fail when those features are implemented (as in
    the case of `GTBlameOptionsTrackCopiesAnyCommitCopies`)  or I just
    plain didn’t test them (As in the case of
    `GTBlameOptionsMinimumMatchCharacters`).

--- ObjectiveGitTests/GTBlameSpec.m
+++ ObjectiveGitTests/GTBlameSpec.m
@@ -58,4 +58,38 @@ it(@"should be able to get the same hunk from an index or a line", ^{
 	expect(hunk).to.equal(lineHunk);
 });
 
+describe(@"Creating a blame with options", ^{
+	it(@"should follow the instructions provided by the GTBlameOptionsOldestCommitOID key", ^{
+		GTOID *oldOID = [GTOID oidWithSHA:@"1d69f3c0aeaf0d62e25591987b93b8ffc53abd77"];
+		GTBlame *optionsBlame = [GTBlame blameWithFile:@"README1.txt" inRepository:self.testAppFixtureRepository options:@{GTBlameOptionsOldestCommitOID: oldOID } error:nil];
+
+		expect([optionsBlame hunkAtIndex:0].originalCommitOID).to.equal(oldOID);
+		expect([blame hunkAtIndex:0].originalCommitOID).toNot.equal(oldOID);
+	});
+	
+	it(@"should follow the instructions provided by the GTBlameOptionsNewestCommitOID key", ^{
+		GTOID *newOID = [GTOID oidWithSHA:@"6317779b4731d9c837dcc6972b964bdf4211eeef"];
+		GTBlame *optionsBlame = [GTBlame blameWithFile:@"README1.txt" inRepository:self.testAppFixtureRepository options:@{GTBlameOptionsNewestCommitOID: newOID } error:nil];
+		expect(optionsBlame.hunkCount).to.equal(1);
+		expect(blame.hunkCount).to.equal(4);
+	});
+	
+	it(@"should follow the instructions provided by the GTBlameOptionsFirstLine key", ^{
+		GTBlame *optionsBlame = [GTBlame blameWithFile:@"README1.txt" inRepository:self.testAppFixtureRepository options:@{ GTBlameOptionsFirstLine: @22, GTBlameOptionsLastLine: @24 } error:nil];
+		expect(optionsBlame.hunkCount).toNot.equal(blame.hunkCount);
+	});
+	
+	it(@"should follow the instructions provided by the GTBlameOptionsTrackCopiesAnyCommitCopies key", ^{
+		GTBlame *optionsBlame = [GTBlame blameWithFile:@"README_renamed" inRepository:self.testAppFixtureRepository options:@{ GTBlameOptionsFlags: @(GTBlameOptionsTrackCopiesAnyCommitCopies) } error:nil];
+
+		[optionsBlame enumerateHunksUsingBlock:^(GTBlameHunk *hunk, BOOL *stop) {
+			expect(hunk.originalPath).to.equal(@"README");
+			// These test should fail when `GTBlameOptionsTrackCopiesAnyCommitCopies` is implemented.
+			expect(hunk.finalCommitOID).to.equal(hunk.originalCommitOID);
+			expect(hunk.originalSignature).to.equal(hunk.finalSignature);
+			expect(hunk.originalStartLineNumber).to.equal(hunk.finalStartLineNumber);
+		}];
+	});
+});
+
 SpecEnd
