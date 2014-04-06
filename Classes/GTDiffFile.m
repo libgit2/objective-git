13d44a09389f1e8fba64a3345dd24088357b9e86
Alan Rogers <alan@github.com>
2014-04-06 15:12:03 -0500

    Return nil if we can't create a file system path.

--- Classes/GTDiffFile.m
+++ Classes/GTDiffFile.m
@@ -16,16 +16,15 @@
 
 	self = [super init];
 	if (self == nil) return nil;
-	
+
+	_path = [NSFileManager.defaultManager stringWithFileSystemRepresentation:file.path length:strlen(file.path)];
+	if (_path == nil) return nil;
+
 	_git_diff_file = file;
-	
 	_size = (NSUInteger)file.size;
 	_flags = (GTDiffFileFlag)file.flags;
 	_mode = file.mode;
 
-	_path = [NSFileManager.defaultManager stringWithFileSystemRepresentation:file.path length:strlen(file.path)];
-	NSParameterAssert(_path != nil);
-	
 	return self;
 }
 
