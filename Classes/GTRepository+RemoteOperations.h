defc9066d2e66a0cf875137f440103dbf175a449
Pablo Bendersky <pbendersky@quadiontech.com>
2014-08-15 13:23:29 -0300

    Added method to enumerate fetch head entries.

--- Classes/GTRepository+RemoteOperations.h
+++ Classes/GTRepository+RemoteOperations.h
@@ -11,6 +11,8 @@
 // A `GTCredentialProvider`, that will be used to authenticate against the remote.
 extern NSString *const GTRepositoryRemoteOptionsCredentialProvider;
 
+@class GTFetchHeadEntry;
+
 @interface GTRepository (RemoteOperations)
 
 // Fetch a remote.
@@ -26,9 +28,20 @@ extern NSString *const GTRepositoryRemoteOptionsCredentialProvider;
 // will point to an error describing what happened).
 - (BOOL)fetchRemote:(GTRemote *)remote withOptions:(NSDictionary *)options error:(NSError **)error progress:(void (^)(const git_transfer_progress *stats, BOOL *stop))progressBlock;
 
-// Returns an array of GTFetchHeadEntry objects containing the entries in the FETCH_HEAD file.
+// Enumerate all available fetch head entries.
 //
 // error - The error if one ocurred. Can be NULL.
+// block - A block to execute for each FETCH_HEAD entry. `fetchHeadEntry` will be the current
+//         fetch head entry. Setting `stop` to YES will cause enumeration to stop after the block returns.
+//
+// Returns YES if the operation succedded, NO otherwise.
+- (BOOL)enumerateFetchHeadEntriesWithError:(NSError **)error usingBlock:(void (^)(GTFetchHeadEntry *fetchHeadEntry, BOOL *stop))block;
+
+// Convenience method for -enumerateFetchHeadEntriesWithError:usingBlock: that retuns an NSArray with all the fetch head entries.
+//
+// error - The error if one ocurred. Can be NULL.
+//
+// Retruns an array with GTFetchHeadEntry objects
 - (NSArray *)fetchHeadEntriesWithError:(NSError **)error;
 
 @end
--- Classes/GTRepository+RemoteOperations.m
+++ Classes/GTRepository+RemoteOperations.m
@@ -40,22 +40,30 @@ int GTRemoteFetchTransferProgressCallback(const git_transfer_progress *stats, vo
 	return (stop == YES ? GIT_EUSER : 0);
 }
 
+typedef void (^GTRemoteEnumerateFetchHeadEntryBlock)(GTFetchHeadEntry *entry, BOOL *stop);
+
 typedef struct {
-	__unsafe_unretained NSMutableArray *entries;
 	__unsafe_unretained GTRepository *repository;
-} GTFetchHeadEntriesPayload;
+	__unsafe_unretained GTRemoteEnumerateFetchHeadEntryBlock enumerationBlock;
+} GTEnumerateHeadEntriesPayload;
 
 int GTFetchHeadEntriesCallback(const char *ref_name, const char *remote_url, const git_oid *oid, unsigned int is_merge, void *payload) {
-	GTFetchHeadEntriesPayload *entriesPayload = payload;
+	GTEnumerateHeadEntriesPayload *entriesPayload = payload;
 	
 	GTRepository *repository = entriesPayload->repository;
+	GTRemoteEnumerateFetchHeadEntryBlock enumerationBlock = entriesPayload->enumerationBlock;
 	
 	GTReference *reference = [GTReference referenceByLookingUpReferencedNamed:@(ref_name) inRepository:repository error:NULL];
 	
 	GTFetchHeadEntry *entry = [GTFetchHeadEntry fetchEntryWithReference:reference remoteURL:@(remote_url) targetOID:[GTOID oidWithGitOid:oid] isMerge:(BOOL)is_merge];
-	[entriesPayload->entries addObject:entry];
 	
-	return 0;
+	BOOL stop = NO;
+	
+	if (enumerationBlock) {
+		enumerationBlock(entry, &stop);
+	}
+	
+	return (int)stop;
 }
 
 #pragma mark -
@@ -92,18 +100,29 @@ int GTFetchHeadEntriesCallback(const char *ref_name, const char *remote_url, con
 	}
 }
 
-- (NSArray *)fetchHeadEntriesWithError:(NSError **)error {
-	NSMutableArray *entries = [NSMutableArray array];
-	GTFetchHeadEntriesPayload payload = {
-		.entries = entries,
+- (BOOL)enumerateFetchHeadEntriesWithError:(NSError **)error usingBlock:(void (^)(GTFetchHeadEntry *fetchHeadEntry, BOOL *stop))block {
+	GTEnumerateHeadEntriesPayload payload = {
 		.repository = self,
+		.enumerationBlock = block,
 	};
-
 	int gitError = git_repository_fetchhead_foreach(self.git_repository, GTFetchHeadEntriesCallback, &payload);
+
 	if (gitError != GIT_OK) {
 		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to get fetchhead entries"];
-		return nil;
+		return NO;
 	}
+	
+	return YES;
+}
+
+- (NSArray *)fetchHeadEntriesWithError:(NSError **)error {
+	__block NSMutableArray *entries = [NSMutableArray array];
+	
+	[self enumerateFetchHeadEntriesWithError:error usingBlock:^(GTFetchHeadEntry *fetchHeadEntry, BOOL *stop) {
+		[entries addObject:fetchHeadEntry];
+		
+		*stop = NO;
+	}];
 	
 	return [entries copy];
 }
