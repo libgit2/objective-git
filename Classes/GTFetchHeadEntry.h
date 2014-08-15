6f48fe83e811d867a143326d2fb706df7ebf6fd5
Pablo Bendersky <pbendersky@quadiontech.com>
2014-08-15 14:58:49 -0300

    Added a third / to comments (see #402)

--- Classes/GTFetchHeadEntry.h
+++ Classes/GTFetchHeadEntry.h
@@ -12,37 +12,37 @@
 @class GTOID;
 @class GTReference;
 
-// A class representing an entry on the FETCH_HEAD file, as returned by the callback of git_repository_fetchhead_foreach.
+/// A class representing an entry on the FETCH_HEAD file, as returned by the callback of git_repository_fetchhead_foreach.
 @interface GTFetchHeadEntry : NSObject
 
-// The reference of this fetch entry.
+/// The reference of this fetch entry.
 @property (nonatomic, readonly, strong) GTReference *reference;
 
-// The remote URL where this entry was originally fetched from.
+/// The remote URL where this entry was originally fetched from.
 @property (nonatomic, readonly, copy) NSString *remoteURL;
 
-// The target OID of this fetch entry (what we need to merge with)
+/// The target OID of this fetch entry (what we need to merge with)
 @property (nonatomic, readonly, strong) GTOID *targetOID;
 
-// Flag indicating if we need to merge this entry or not.
+/// Flag indicating if we need to merge this entry or not.
 @property (nonatomic, getter = isMerge, readonly) BOOL merge;
 
-// Initializes a GTFetchHeadEntry.
-//
-// reference - Reference on the repository
-// remoteURL - URL where this was originally fetched from
-// targetOID - Target OID
-// merge - Indicates if this is pending a merge.
+/// Initializes a GTFetchHeadEntry.
+///
+/// reference - Reference on the repository
+/// remoteURL - URL where this was originally fetched from
+/// targetOID - Target OID
+/// merge - Indicates if this is pending a merge.
 - (instancetype)initWithReference:(GTReference *)reference remoteURL:(NSString *)remoteURL targetOID:(GTOID *)targetOID isMerge:(BOOL)merge;
 
-// Returns a new GTFetchHeadEntry instance.
-//
-// reference - Reference on the repository
-// remoteURL - URL where this was originally fetched from
-// targetOID - Target OID
-// merge - Indicates if this is pending a merge.
-//
-// Returns a new GTFetchHeadEntry already initialized.
+/// Returns a new GTFetchHeadEntry instance.
+///
+/// reference - Reference on the repository
+/// remoteURL - URL where this was originally fetched from
+/// targetOID - Target OID
+/// merge - Indicates if this is pending a merge.
+///
+/// Returns a new GTFetchHeadEntry already initialized.
 + (instancetype)fetchEntryWithReference:(GTReference *)reference remoteURL:(NSString *)remoteURL targetOID:(GTOID *)targetOID isMerge:(BOOL)merge;
 
 @end
