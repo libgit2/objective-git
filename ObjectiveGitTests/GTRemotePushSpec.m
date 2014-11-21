c947b8036e11a2e456ab85b62dbebc06575c761e
Ben Chatelain <benchatelain@gmail.com>
2014-11-20 23:36:14 -0700

    Clean up push tests

--- ObjectiveGitTests/GTRemotePushSpec.m
+++ ObjectiveGitTests/GTRemotePushSpec.m
@@ -12,163 +12,164 @@
 
 #import "QuickSpec+GTFixtures.h"
 
+// Helper to quickly create commits
+GTCommit *(^createCommitInRepository)(NSString *, NSData *, NSString *, GTRepository *) = ^(NSString *message, NSData *fileData, NSString *fileName, GTRepository *repo) {
+	GTTreeBuilder *treeBuilder = [[GTTreeBuilder alloc] initWithTree:nil error:nil];
+	[treeBuilder addEntryWithData:fileData fileName:fileName fileMode:GTFileModeBlob error:nil];
+
+	GTTree *testTree = [treeBuilder writeTreeToRepository:repo error:nil];
+
+	// We need the parent commit to make the new one
+	GTReference *headReference = [repo headReferenceWithError:nil];
+
+	GTEnumerator *commitEnum = [[GTEnumerator alloc] initWithRepository:repo error:nil];
+	[commitEnum pushSHA:[headReference targetSHA] error:nil];
+	GTCommit *parent = [commitEnum nextObject];
+
+	GTCommit *testCommit = [repo createCommitWithTree:testTree message:message parents:@[parent] updatingReferenceNamed:headReference.name error:nil];
+	expect(testCommit).notTo(beNil());
+
+	return testCommit;
+};
+
+#pragma mark - GTRemotePushSpec
+
 QuickSpecBegin(GTRemotePushSpec)
 
-describe(@"push to remote", ^{
+describe(@"pushing", ^{
 	__block GTRepository *localRepo;
 	__block GTRepository *remoteRepo;
+	__block	GTRepository *notBareRepo;
 	__block GTRemote *remote;
-	__block NSURL *notBareRepoURL;
-	__block NSURL *remoteRepoFileURL;
+	__block NSURL *remoteRepoURL;
 	__block NSURL *localRepoURL;
 	__block GTBranch *masterBranch;
 	__block GTBranch *remoteMasterBranch;
+	__block	NSError *error = nil;
 
 	beforeEach(^{
-		NSError *error = nil;
-
 		// This repo is not really "bare"
-		GTRepository *notBareRepo = self.bareFixtureRepository;
+		notBareRepo = self.bareFixtureRepository;
 		expect(notBareRepo).notTo(beNil());
 		expect(@(notBareRepo.isBare)).to(beFalse());
-
-		// Make a bare clone to serve as the remote
-		notBareRepoURL = [notBareRepo.gitDirectoryURL.URLByDeletingLastPathComponent URLByAppendingPathComponent:@"barerepo.git"];
-		NSDictionary *options = @{ GTRepositoryCloneOptionsBare: @(1) };
-		remoteRepo = [GTRepository cloneFromURL:notBareRepo.gitDirectoryURL toWorkingDirectory:notBareRepoURL options:options error:&error transferProgressBlock:NULL checkoutProgressBlock:NULL];
-		expect(error).to(beNil());
-		expect(remoteRepo).notTo(beNil());
-		expect(@(remoteRepo.isBare)).to(beTruthy()); // that's better
-
-		NSArray *remoteBranches = [remoteRepo localBranchesWithError:&error];
-		expect(error).to(beNil());
-		expect(remoteBranches).notTo(beNil());
-		expect(@(remoteBranches.count)).to(beGreaterThanOrEqualTo(@1));
-
-		remoteMasterBranch = remoteBranches[0];
-		expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
-
-		NSURL *remoteRepoFileURL = remoteRepo.gitDirectoryURL;
-		expect(remoteRepoFileURL).notTo(beNil());
-		NSURL *localRepoURL = [remoteRepoFileURL.URLByDeletingLastPathComponent URLByAppendingPathComponent:@"localpushrepo"];
-		expect(localRepoURL).notTo(beNil());
-
-		// Ensure repo destination is clear before clone
-		[NSFileManager.defaultManager removeItemAtURL:localRepoURL error:NULL];
-
-		// Local clone for testing pushes
-		localRepo = [GTRepository cloneFromURL:remoteRepoFileURL toWorkingDirectory:localRepoURL options:nil error:&error transferProgressBlock:NULL checkoutProgressBlock:NULL];
-
-		expect(error).to(beNil());
-		expect(localRepo).notTo(beNil());
-
-		GTConfiguration *configuration = [localRepo configurationWithError:&error];
-		expect(error).to(beNil());
-		expect(configuration).notTo(beNil());
-
-		expect(@(configuration.remotes.count)).to(equal(@1));
-
-		remote = configuration.remotes[0];
-		expect(remote.name).to(equal(@"origin"));
-
-		NSArray *branches = [localRepo localBranchesWithError:&error];
-		expect(error).to(beNil());
-		expect(branches).notTo(beNil());
-		expect(@(branches.count)).to(beGreaterThanOrEqualTo(@1));
-
-		masterBranch = branches[0];
-		expect(masterBranch.shortName).to(equal(@"master"));
-		expect(@([masterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
 	});
 
-	afterEach(^{
-		[NSFileManager.defaultManager removeItemAtURL:notBareRepoURL error:NULL];
-		[NSFileManager.defaultManager removeItemAtURL:remoteRepoFileURL error:NULL];
-		[NSFileManager.defaultManager removeItemAtURL:localRepoURL error:NULL];
-	});
-
-	// Helper to quickly create commits
-	GTCommit *(^createCommitInRepository)(NSString *, NSData *, NSString *, GTRepository *) = ^(NSString *message, NSData *fileData, NSString *fileName, GTRepository *repo) {
-		GTTreeBuilder *treeBuilder = [[GTTreeBuilder alloc] initWithTree:nil error:nil];
-		[treeBuilder addEntryWithData:fileData fileName:fileName fileMode:GTFileModeBlob error:nil];
-
-		GTTree *testTree = [treeBuilder writeTreeToRepository:repo error:nil];
-
-		// We need the parent commit to make the new one
-		GTReference *headReference = [repo headReferenceWithError:nil];
-
-		GTEnumerator *commitEnum = [[GTEnumerator alloc] initWithRepository:repo error:nil];
-		[commitEnum pushSHA:[headReference targetSHA] error:nil];
-		GTCommit *parent = [commitEnum nextObject];
-
-		GTCommit *testCommit = [repo createCommitWithTree:testTree message:message parents:@[parent] updatingReferenceNamed:headReference.name error:nil];
-		expect(testCommit).notTo(beNil());
-
-		return testCommit;
-	};
-
-	describe(@"-pushBranch:toRemote:withOptions:error:progress:", ^{
-		it(@"pushes nothing when the branch on local and remote are in sync", ^{
-			NSError *error = nil;
-
-			expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
-
-			__block BOOL transferProgressed = NO;
-			BOOL result = [localRepo pushBranch:masterBranch toRemote:remote withOptions:nil error:&error progress:^(unsigned int current, unsigned int total, size_t bytes, BOOL *stop) {
-				transferProgressed = YES;
-			}];
+	describe(@"to remote", ^{	// via local transport
+		beforeEach(^{
+			// Make a bare clone to serve as the remote
+			remoteRepoURL = [notBareRepo.gitDirectoryURL.URLByDeletingLastPathComponent URLByAppendingPathComponent:@"bare_remote_repo.git"];
+			NSDictionary *options = @{ GTRepositoryCloneOptionsBare: @1 };
+			remoteRepo = [GTRepository cloneFromURL:notBareRepo.gitDirectoryURL toWorkingDirectory:remoteRepoURL options:options error:&error transferProgressBlock:NULL checkoutProgressBlock:NULL];
 			expect(error).to(beNil());
-			expect(@(result)).to(beTruthy());
-			expect(@(transferProgressed)).to(beFalse()); // Local transport doesn't currently call progress callbacks
+			expect(remoteRepo).notTo(beNil());
+			expect(@(remoteRepo.isBare)).to(beTruthy()); // that's better
 
-			// Same number of commits after push
+			// Get the remote master branch
+			NSArray *remoteBranches = [remoteRepo localBranchesWithError:&error];
+			expect(error).to(beNil());
+			expect(remoteBranches).notTo(beNil());
+			expect(@(remoteBranches.count)).to(beGreaterThanOrEqualTo(@1));
+			remoteMasterBranch = remoteBranches[0];
 			expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
+
+			NSURL *localRepoURL = [remoteRepoURL.URLByDeletingLastPathComponent URLByAppendingPathComponent:@"local_push_repo"];
+			expect(localRepoURL).notTo(beNil());
+
+			// Ensure repo destination is clear before clone
+			[NSFileManager.defaultManager removeItemAtURL:localRepoURL error:NULL];
+
+			// Local clone for testing pushes
+			localRepo = [GTRepository cloneFromURL:remoteRepoURL toWorkingDirectory:localRepoURL options:nil error:&error transferProgressBlock:NULL checkoutProgressBlock:NULL];
+
+			expect(error).to(beNil());
+			expect(localRepo).notTo(beNil());
+
+			GTConfiguration *configuration = [localRepo configurationWithError:&error];
+			expect(error).to(beNil());
+			expect(configuration).notTo(beNil());
+
+			expect(@(configuration.remotes.count)).to(equal(@1));
+
+			remote = configuration.remotes[0];
+			expect(remote.name).to(equal(@"origin"));
+
+			NSArray *branches = [localRepo localBranchesWithError:&error];
+			expect(error).to(beNil());
+			expect(branches).notTo(beNil());
+			expect(@(branches.count)).to(beGreaterThanOrEqualTo(@1));
+
+			masterBranch = branches[0];
+			expect(masterBranch.shortName).to(equal(@"master"));
+			expect(@([masterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
 		});
 
-		it(@"pushes a new local commit to the remote", ^{
-			NSError *error = nil;
+		afterEach(^{
+			[NSFileManager.defaultManager removeItemAtURL:remoteRepoURL error:NULL];
+			[NSFileManager.defaultManager removeItemAtURL:localRepoURL error:NULL];
+		});
 
-			// Create a new commit in the master repo
-			NSString *testData = @"Test";
-			NSString *fileName = @"test.txt";
-			GTCommit *testCommit = createCommitInRepository(@"Test commit", [testData dataUsingEncoding:NSUTF8StringEncoding], fileName, localRepo);
-			expect(testCommit).notTo(beNil());
+		context(@"when the local and remote branches are in sync", ^{
+			it(@"should push no commits", ^{
+				expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
 
-			// Refetch master branch to ensure the commit count is accurate
-			masterBranch = [localRepo localBranchesWithError:NULL][0];
-			expect(@([masterBranch numberOfCommitsWithError:NULL])).to(equal(@4));
+				// Push
+				__block BOOL transferProgressed = NO;
+				BOOL result = [localRepo pushBranch:masterBranch toRemote:remote withOptions:nil error:&error progress:^(unsigned int current, unsigned int total, size_t bytes, BOOL *stop) {
+					transferProgressed = YES;
+				}];
+				expect(error).to(beNil());
+				expect(@(result)).to(beTruthy());
+				expect(@(transferProgressed)).to(beFalse()); // Local transport doesn't currently call progress callbacks
 
-			// Number of commits on remote before push
-			expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
+				// Same number of commits after push
+				expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
+			});
+		});
 
-			// Push
-			__block BOOL transferProgressed = NO;
-			BOOL result = [localRepo pushBranch:masterBranch toRemote:remote withOptions:nil error:&error progress:^(unsigned int current, unsigned int total, size_t bytes, BOOL *stop) {
-				transferProgressed = YES;
-			}];
-			expect(error).to(beNil());
-			expect(@(result)).to(beTruthy());
-			expect(@(transferProgressed)).to(beFalse()); // Local transport doesn't currently call progress callbacks
+		context(@"when there is a new local commit", ^{
+			it(@"should push one commit", ^{
+				// Create a new commit in the master repo
+				NSString *testData = @"Test";
+				NSString *fileName = @"test.txt";
+				GTCommit *testCommit = createCommitInRepository(@"Test commit", [testData dataUsingEncoding:NSUTF8StringEncoding], fileName, localRepo);
+				expect(testCommit).notTo(beNil());
 
-			// Refetch master branch to ensure the commit count is accurate
-			remoteMasterBranch = [remoteRepo localBranchesWithError:NULL][0];
+				// Refetch master branch to ensure the commit count is accurate
+				masterBranch = [localRepo localBranchesWithError:NULL][0];
+				expect(@([masterBranch numberOfCommitsWithError:NULL])).to(equal(@4));
 
-			// Number of commits on remote after push
-			expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@4));
+				// Number of commits on remote before push
+				expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@3));
 
-			// Verify commit is in remote
-			GTCommit *pushedCommit = [remoteRepo lookUpObjectByOID:testCommit.OID objectType:GTObjectTypeCommit error:&error];
-			expect(error).to(beNil());
-			expect(pushedCommit).notTo(beNil());
-			expect(pushedCommit.OID).to(equal(testCommit.OID));
+				// Push
+				__block BOOL transferProgressed = NO;
+				BOOL result = [localRepo pushBranch:masterBranch toRemote:remote withOptions:nil error:&error progress:^(unsigned int current, unsigned int total, size_t bytes, BOOL *stop) {
+					transferProgressed = YES;
+				}];
+				expect(error).to(beNil());
+				expect(@(result)).to(beTruthy());
+				expect(@(transferProgressed)).to(beFalse()); // Local transport doesn't currently call progress callbacks
 
-			GTTreeEntry *entry = [[pushedCommit tree] entryWithName:fileName];
-			expect(entry).notTo(beNil());
+				// Refetch master branch to ensure the commit count is accurate
+				remoteMasterBranch = [remoteRepo localBranchesWithError:NULL][0];
 
-			GTBlob *fileData = (GTBlob *)[entry GTObject:&error];
-			expect(error).to(beNil());
-			expect(fileData).notTo(beNil());
-			expect(fileData.content).to(equal(testData));
+				// Number of commits on remote after push
+				expect(@([remoteMasterBranch numberOfCommitsWithError:NULL])).to(equal(@4));
+
+				// Verify commit is in remote
+				GTCommit *pushedCommit = [remoteRepo lookUpObjectByOID:testCommit.OID objectType:GTObjectTypeCommit error:&error];
+				expect(error).to(beNil());
+				expect(pushedCommit).notTo(beNil());
+				expect(pushedCommit.OID).to(equal(testCommit.OID));
+
+				GTTreeEntry *entry = [[pushedCommit tree] entryWithName:fileName];
+				expect(entry).notTo(beNil());
+
+				GTBlob *fileData = (GTBlob *)[entry GTObject:&error];
+				expect(error).to(beNil());
+				expect(fileData).notTo(beNil());
+				expect(fileData.content).to(equal(testData));
+			});
 		});
 	});
 
