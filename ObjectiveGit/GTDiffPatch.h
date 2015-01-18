3337fbde56b457515d0f799b7d80c2c25552db45
Jan Gassen <jan.gassen@centerdevice.com>
2015-01-18 12:25:45 +0100

    Added git_patch_to_buf method

--- ObjectiveGit/GTDiffPatch.h
+++ ObjectiveGit/GTDiffPatch.h
@@ -50,6 +50,9 @@
 /// Returns the raw size of the delta, in bytes.
 - (NSUInteger)sizeWithContext:(BOOL)includeContext hunkHeaders:(BOOL)includeHunkHeaders fileHeaders:(BOOL)includeFileHeaders;
 
+/// Returns the raw patch data as buffer.
+- (NSData*)toBuffer;
+
 /// Enumerate the hunks contained in the patch.
 ///
 /// This enumeration is synchronous, and will block the calling thread while
