c8971115c90cd0f3827031b8938aa3bf0bb0ee1b
Jan Gassen <jan.gassen@centerdevice.com>
2015-01-20 20:26:04 +0100

    Added null check

--- ObjectiveGit/GTTree.m
+++ ObjectiveGit/GTTree.m
@@ -72,7 +72,7 @@ typedef struct GTTreeEnumerationStruct {
 	git_tree_entry *entry = NULL;
 	int gitError = git_tree_entry_bypath(&entry, self.git_tree, path.UTF8String);
 	if (error != GIT_OK) {
-		*error = [NSError git_errorFor:gitError description:@"Failed to get tree ntry %@", path];
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to get tree ntry %@", path];
 		return nil;
 	}
 	
