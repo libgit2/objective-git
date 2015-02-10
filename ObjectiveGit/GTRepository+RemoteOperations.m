fe680652547c74f57c90b5b0d32a298c7d3bff5c
Ben Chatelain <benchatelain@gmail.com>
2015-02-10 07:47:49 -0700

    API changes for push in libgit2 0.22

--- ObjectiveGit/GTRepository+RemoteOperations.m
+++ ObjectiveGit/GTRepository+RemoteOperations.m
@@ -15,6 +15,7 @@
 #import "GTOID.h"
 #import "GTRemote.h"
 #import "GTSignature.h"
+#import "NSArray+StringArray.h"
 #import "NSError+Git.h"
 
 #import "git2/errors.h"
@@ -157,6 +158,101 @@ int GTFetchHeadEntriesCallback(const char *ref_name, const char *remote_url, con
 	}];
 	
 	return entries;
+}
+
+#pragma mark - Push (Public)
+
+- (BOOL)pushBranch:(GTBranch *)branch toRemote:(GTRemote *)remote withOptions:(NSDictionary *)options error:(NSError **)error progress:(GTRemotePushTransferProgressBlock)progressBlock {
+	NSParameterAssert(branch != nil);
+	NSParameterAssert(remote != nil);
+
+	return [self pushBranches:@[ branch ] toRemote:remote withOptions:options error:error progress:progressBlock];
+}
+
+- (BOOL)pushBranches:(NSArray *)branches toRemote:(GTRemote *)remote withOptions:(NSDictionary *)options error:(NSError **)error progress:(GTRemotePushTransferProgressBlock)progressBlock {
+	NSParameterAssert(branches != nil);
+	NSParameterAssert(branches.count != 0);
+	NSParameterAssert(remote != nil);
+
+	NSMutableArray *refspecs = nil;
+	// Build refspecs for the passed in branches
+	refspecs = [NSMutableArray arrayWithCapacity:branches.count];
+	for (GTBranch *branch in branches) {
+		// Default remote reference for when branch doesn't exist on remote - create with same short name
+		NSString *remoteBranchReference = [NSString stringWithFormat:@"refs/heads/%@", branch.shortName];
+
+		BOOL success = NO;
+		GTBranch *trackingBranch = [branch trackingBranchWithError:error success:&success];
+
+		if (success && trackingBranch) {
+			// Use remote branch short name from trackingBranch, which could be different
+			// (e.g. refs/heads/master:refs/heads/my_master)
+			remoteBranchReference = [NSString stringWithFormat:@"refs/heads/%@", trackingBranch.shortName];
+		}
+
+		[refspecs addObject:[NSString stringWithFormat:@"refs/heads/%@:%@", branch.shortName, remoteBranchReference]];
+	}
+
+	return [self pushRefspecs:refspecs toRemote:remote withOptions:options error:error progress:progressBlock];
+}
+
+#pragma mark - Push (Private)
+
+- (BOOL)pushRefspecs:(NSArray *)refspecs toRemote:(GTRemote *)remote withOptions:(NSDictionary *)options error:(NSError **)error progress:(GTRemotePushTransferProgressBlock)progressBlock {
+	int gitError;
+	GTCredentialProvider *credProvider = options[GTRepositoryRemoteOptionsCredentialProvider];
+
+	GTRemoteConnectionInfo connectionInfo = {
+		.credProvider = { .credProvider = credProvider },
+		.direction = GIT_DIRECTION_PUSH,
+		.pushProgressBlock = progressBlock,
+	};
+
+	git_remote_callbacks remote_callbacks = GIT_REMOTE_CALLBACKS_INIT;
+	remote_callbacks.credentials = (credProvider != nil ? GTCredentialAcquireCallback : NULL),
+	remote_callbacks.transfer_progress = GTRemoteFetchTransferProgressCallback,
+	remote_callbacks.payload = &connectionInfo,
+
+	gitError = git_remote_set_callbacks(remote.git_remote, &remote_callbacks);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to set callbacks on remote"];
+		return NO;
+	}
+
+	gitError = git_remote_connect(remote.git_remote, GIT_DIRECTION_PUSH);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to connect remote"];
+		return NO;
+	}
+	@onExit {
+		git_remote_disconnect(remote.git_remote);
+		// Clear out callbacks by overwriting with an effectively empty git_remote_callbacks struct
+		git_remote_set_callbacks(remote.git_remote, &((git_remote_callbacks)GIT_REMOTE_CALLBACKS_INIT));
+	};
+
+	git_push_options push_options = GIT_PUSH_OPTIONS_INIT;
+
+	gitError = git_push_init_options(&push_options, GIT_PUSH_OPTIONS_VERSION);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to init push options"];
+		return NO;
+	}
+
+	const git_strarray git_refspecs = refspecs.git_strarray;
+
+	gitError = git_remote_upload(remote.git_remote, &git_refspecs, &push_options);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Push upload to remote failed"];
+		return NO;
+	}
+
+	gitError = git_remote_update_tips(remote.git_remote, self.userSignatureForNow.git_signature, NULL);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Update tips failed"];
+		return NO;
+	}
+
+	return YES;
 }
 
 @end
