23c3f46cefe56455b5035cc3f9b21ddee860b90a
Ben Chatelain <benchatelain@gmail.com>
2015-03-23 17:49:47 -0600

    GTDiffHunk nullability

--- ObjectiveGit/GTDiffHunk.h
+++ ObjectiveGit/GTDiffHunk.h
@@ -11,6 +11,8 @@
 @class GTDiffLine;
 @class GTDiffPatch;
 
+NS_ASSUME_NONNULL_BEGIN
+
 /// A class representing a hunk within a diff patch.
 @interface GTDiffHunk : NSObject
 
@@ -24,7 +26,12 @@
 ///
 /// The contents of a hunk are lazily loaded, therefore we initialise the object
 /// simply with the patch it originates from and which hunk index it represents.
-- (instancetype)initWithPatch:(GTDiffPatch *)patch hunkIndex:(NSUInteger)hunkIndex NS_DESIGNATED_INITIALIZER;
+///
+/// patch     - The patch the hunk originates from. Must not be nil.
+/// hunkIndex - The hunk's index within the patch.
+///
+/// Returns the initialized instance.
+- (nullable instancetype)initWithPatch:(GTDiffPatch *)patch hunkIndex:(NSUInteger)hunkIndex NS_DESIGNATED_INITIALIZER;
 
 /// Perfoms the given block on each line in the hunk.
 ///
@@ -32,9 +39,12 @@
 ///
 /// error - A pointer to an NSError that will be set if one occurs.
 /// block - A block to execute on each line. Setting `stop` to `NO` will
-///         immediately stop the enumeration and return from the method.
+///         immediately stop the enumeration and return from the method. Must not
+///         be nil.
 /// Return YES if the enumeration was successful, NO otherwise (and an error will
 /// be set in `error`).
 - (BOOL)enumerateLinesInHunk:(NSError **)error usingBlock:(void (^)(GTDiffLine *line, BOOL *stop))block;
 
 @end
+
+NS_ASSUME_NONNULL_END
