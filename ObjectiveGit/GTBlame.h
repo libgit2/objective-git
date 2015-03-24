f69ac1e35478b8cfe5703f33acf18abea8e923fa
Ben Chatelain <benchatelain@gmail.com>
2015-03-23 18:11:12 -0600

    GTBlame nullability

--- ObjectiveGit/GTBlame.h
+++ ObjectiveGit/GTBlame.h
@@ -12,11 +12,17 @@
 @class GTBlameHunk;
 @class GTRepository;
 
-/// A `GTBlame` provides authorship info, through `GTBlameHunk` for each line of a file.
+NS_ASSUME_NONNULL_BEGIN
+
+/// A `GTBlame` provides authorship info, through `GTBlameHunk` for each line of a file. Analogous to `git_blame` in libgit2.
 @interface GTBlame : NSObject
 
 /// Designated initializer.
-- (instancetype)initWithGitBlame:(git_blame *)blame NS_DESIGNATED_INITIALIZER;
+///
+/// blame - A git_blame to wrap. May not be NULL.
+///
+/// Returns a blame, or nil if initialization failed.
+- (nullable instancetype)initWithGitBlame:(git_blame *)blame NS_DESIGNATED_INITIALIZER;
 
 /// Get all the hunks in the blame. A convenience wrapper around `enumerateHunksUsingBlock:`
 @property (nonatomic, strong, readonly) NSArray *hunks;
@@ -29,12 +35,13 @@
 /// index - The index to retrieve the hunk from.
 ///
 /// Returns a `GTBlameHunk` or nil if an error occurred.
-- (GTBlameHunk *)hunkAtIndex:(NSUInteger)index;
+- (nullable GTBlameHunk *)hunkAtIndex:(NSUInteger)index;
 
 /// Enumerate the hunks in the blame.
 ///
 /// block - A block invoked for every hunk in the blame.
 ///         Setting stop to `YES` instantly stops the enumeration.
+///         May not be NULL.
 ///
 - (void)enumerateHunksUsingBlock:(void (^)(GTBlameHunk *hunk, NSUInteger index, BOOL *stop))block;
 
@@ -43,10 +50,11 @@
 /// lineNumber - The (1 based) line number to find a hunk for.
 ///
 /// Returns a `GTBlameHunk` or nil if an error occurred.
-- (GTBlameHunk *)hunkAtLineNumber:(NSUInteger)lineNumber;
+- (nullable GTBlameHunk *)hunkAtLineNumber:(NSUInteger)lineNumber;
 
 /// The underlying `git_blame` object.
 - (git_blame *)git_blame __attribute__((objc_returns_inner_pointer));
 
 @end
 
+NS_ASSUME_NONNULL_END
