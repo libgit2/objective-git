f37d97e1d460903329f00cf7fe40def2a752d0b8
joshaber <joshaber@gmail.com>
2015-07-03 15:02:57 -0400

    Update tests.

--- ObjectiveGitTests/GTSubmoduleSpec.m
+++ ObjectiveGitTests/GTSubmoduleSpec.m
@@ -93,6 +93,15 @@ it(@"should add its HEAD to its parent's index", ^{
 	expect(@(success)).to(beTruthy());
 });
 
+it(@"should update the ignore rule", ^{
+	GTSubmodule *submodule = [repo submoduleWithName:@"Test_App" error:NULL];
+	expect(submodule).notTo(beNil());
+	expect(@(submodule.ignoreRule)).to(equal(@(GTSubmoduleIgnoreNone)));
+
+	submodule = [submodule submoduleByUpdatingIgnoreRule:GTSubmoduleIgnoreAll error:NULL];
+	expect(@(submodule.ignoreRule)).to(equal(@(GTSubmoduleIgnoreAll)));
+});
+
 describe(@"clean, checked out submodule", ^{
 	__block GTSubmodule *submodule;
 
@@ -204,13 +213,11 @@ describe(@"dirty, checked out submodule", ^{
 	});
 
 	it(@"should honor the ignore rule", ^{
-		submodule.ignoreRule = GTSubmoduleIgnoreDirty;
-
 		GTSubmoduleStatus expectedStatus =
 			GTSubmoduleStatusExistsInHEAD | GTSubmoduleStatusExistsInIndex | GTSubmoduleStatusExistsInConfig | GTSubmoduleStatusExistsInWorkingDirectory |
 			GTSubmoduleStatusModifiedInIndex | GTSubmoduleStatusModifiedInWorkingDirectory;
 
-		expect(@([submodule status:NULL])).to(equal(@(expectedStatus)));
+		expect(@([submodule statusWithIgnoreRule:GTSubmoduleIgnoreDirty error:NULL])).to(equal(@(expectedStatus)));
 	});
 
 	it(@"should open a repository" ,^{
