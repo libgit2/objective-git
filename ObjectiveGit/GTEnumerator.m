893d4209945e10786e6ed3bcd5c36c96082140a4
Etienne Samson <samson.etienne@gmail.com>
2016-03-25 13:05:22 +0000

    Add push/hide HEAD/refname methods to GTEnumerator

--- ObjectiveGit/GTEnumerator.m
+++ ObjectiveGit/GTEnumerator.m
@@ -107,6 +107,26 @@
 	return YES;
 }
 
+- (BOOL)pushHEAD:(NSError **)error {
+	int gitError = git_revwalk_push_head(self.walk);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to push HEAD onto rev walker."];
+		return NO;
+	}
+	return YES;
+}
+
+- (BOOL)pushReferenceName:(NSString *)refName error:(NSError **)error {
+	NSParameterAssert(refName != nil);
+
+	int gitError = git_revwalk_push_ref(self.walk, refName.UTF8String);
+	if (gitError != 0) {
+		if (error) *error = [NSError git_errorFor:gitError description:@"Failed to push reference %@", refName];
+		return NO;
+	}
+	return YES;
+}
+
 - (BOOL)hideSHA:(NSString *)sha error:(NSError **)error {
 	NSParameterAssert(sha != nil);
 
@@ -131,6 +151,26 @@
 		return NO;
 	}
 	
+	return YES;
+}
+
+- (BOOL)hideHEAD:(NSError **)error {
+	int gitError = git_revwalk_hide_head(self.walk);
+	if (gitError != GIT_OK) {
+		if (error != NULL) *error = [NSError git_errorFor:gitError description:@"Failed to hide HEAD onto rev walker."];
+		return NO;
+	}
+	return YES;
+}
+
+- (BOOL)hideReferenceName:(NSString *)refName error:(NSError **)error {
+	NSParameterAssert(refName != nil);
+
+	int gitError = git_revwalk_hide_ref(self.walk, refName.UTF8String);
+	if (gitError != 0) {
+		if (error) *error = [NSError git_errorFor:gitError description:@"Failed to hide reference %@", refName];
+		return NO;
+	}
 	return YES;
 }
 
